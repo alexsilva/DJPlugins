ba48700556f20fa5c190722614eeafcc19a53edd
alexssb <geniofuturo@gmail.com>
2013-05-31 20:17:24 -0300

    Making patterns of the app urls added, are recognized at runtime.

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,9 +4,17 @@ from plugins.models import App
 
 from django.conf import settings
 from plugins.validate import app_exists
-
+import sys
 
 class AppAdmin(admin.ModelAdmin):
+
+    def force_url_conf_reload(self):
+        """
+        Remove the url conf standard sys.module makes the url patterns are
+        recharged and the urls of the app (plugin) are recognized at runtime.
+        """
+        if sys.modules.has_key(settings.ROOT_URLCONF):
+            del sys.modules[settings.ROOT_URLCONF]
 
     def add_configure(self, obj):
         if app_exists(obj.name) and not obj.name in settings.INSTALLED_APPS:
@@ -14,8 +22,9 @@ class AppAdmin(admin.ModelAdmin):
             settings._wrapped.INSTALLED_APPS += (obj.name, ) # hardcore change!
         else:
             print "LOG: settings::add fail"
-            for name in settings.INSTALLED_APPS:
-                print name
+
+        # Force reloading the url conf standard.
+        self.force_url_conf_reload()
 
     def del_configure(self, obj):
         if obj.name in settings.INSTALLED_APPS:
@@ -27,6 +36,9 @@ class AppAdmin(admin.ModelAdmin):
             settings._wrapped.INSTALLED_APPS = apps # hardcore change!
         else:
             print "LOG: settings::delete fail"
+
+        # Force reloading the url conf standard.
+        self.force_url_conf_reload()
 
     def save_model(self, request, obj, form, change):
         super(AppAdmin,self).save_model(request, obj, form, change)
