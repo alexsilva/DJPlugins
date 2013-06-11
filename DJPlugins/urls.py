13cc58c4e2b047237b70ea4ba63ea3bfec39e528
alexssb <geniofuturo@gmail.com>
2013-06-10 21:13:58 -0300

    Configuring urls for the site templates.

--- DJPlugins/urls.py
+++ DJPlugins/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, include, url
+from admin.sites import AdminSite, PublicSite
 
 # Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+# Creating two objects websites. but their behavior will be different.
+# The need to rewrite the admin site is in fact apply plugins on it.
+admin.sites.site = admin.site = AdminSite()
+publicSite = PublicSite()
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,6 +18,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^site/', include(publicSite.urls)),
 
     url(r'^plugins/', include("plugins.urls")),
 )
